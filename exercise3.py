4f01c1f9b0e735743f713fcd43a57a99d713e24b
SLiana <liana.sukiasyan@mail.utoronto.ca>
2015-11-05 18:02:00 -0500

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -95,6 +95,22 @@ def difference(table1, table2):
         if tables t1 and t2 don't have the same attributes
     """
     # TODO(@SLiana)
+    new_table = []
+
+    if table1[0] == table2[0]:
+        # Append header row
+        new_table.append(table1[0])
+
+        # Matching table1 rows to table2
+        for t1_row in table1[1:]:
+            if t1_row not in table2 or t1_row not in new_table:
+                new_table.append(t1_row)
+
+    else:
+        raise MismatchedAttributesException("Bad Schema.")
+
+    return new_table
+    
     dataDiff = []
 
 
@@ -122,54 +138,3 @@ def remove_duplicates(l):
 
 
 # TODO(@SLiana)
-# a version suggestion for Union function
-GRADUATES = [["Number", "Surname", "Age"],
-             [7274, "Robinson", 37],
-             [7432, "O'Malley", 39],
-             [9824, "Darkes", 38]]
-
-MANAGERS = [["Number", "Surname", "Age"],
-            [9297, "O'Malley", 56],
-            [7432, "O'Malley", 39],
-            [9824, "Darkes", 38]]
-            
-dataUnion = []
-dataIntersection = []
-dataDiff = []
-
-def union(t1, t2):
-    global dataUnion
-    if ( len(t1[0]) != len(t2[0]) ):
-        print ("Schema of the tables is not the same");
-        exit();
-    dataUnion = t1
-    unique(t2, t1)
-    print "Union"
-    print(dataUnion)
-    
-def unique(table1, table2):
-    global dataUnion
-    rows = len(table1)
-    rowsT2 = len(table2)
-    rowsI = 0
-    while rowsI < rows:
-        cols = len(table1[rowsI])
-        j = 0
-        match = 0
-        while j < cols:
-            #match = 0
-            dataLenI = 0
-            while dataLenI < rowsT2:
-                if ( str(table1[rowsI][j]) == str(table2[dataLenI][j]) ):
-                    match += 1
-                dataLenI += 1
-
-            j += 1
-        if ( match != cols ):
-            dataUnion.insert(len(dataUnion), table1[rowsI])
-
-        rowsI += 1
-        
-
-union (GRADUATES, MANAGERS)
-#print(sorted(GRADUATES, key=lambda Age: Age[2]))
