a4aed2c6be23eaa861cfa7e3c09c905a06f0e2da
SLiana <liana.sukiasyan@mail.utoronto.ca>
2015-11-05 00:22:52 -0500

    added a a version suggestion for Union function

--- exercise3.py
+++ exercise3.py
@@ -121,4 +121,54 @@ def remove_duplicates(l):
 
 
 
+# a version suggestion for Union function
+GRADUATES = [["Number", "Surname", "Age"],
+             [7274, "Robinson", 37],
+             [7432, "O'Malley", 39],
+             [9824, "Darkes", 38]]
 
+MANAGERS = [["Number", "Surname", "Age"],
+            [9297, "O'Malley", 56],
+            [7432, "O'Malley", 39],
+            [9824, "Darkes", 38]]
+            
+dataUnion = []
+dataIntersection = []
+dataDiff = []
+
+def union(t1, t2):
+    global dataUnion
+    if ( len(t1[0]) != len(t2[0]) ):
+        print ("Schema of the tables is not the same");
+        exit();
+    dataUnion = t1
+    unique(t2, t1)
+    print "Union"
+    print(dataUnion)
+    
+def unique(table1, table2):
+    global dataUnion
+    rows = len(table1)
+    rowsT2 = len(table2)
+    rowsI = 0
+    while rowsI < rows:
+        cols = len(table1[rowsI])
+        j = 0
+        match = 0
+        while j < cols:
+            #match = 0
+            dataLenI = 0
+            while dataLenI < rowsT2:
+                if ( str(table1[rowsI][j]) == str(table2[dataLenI][j]) ):
+                    match += 1
+                dataLenI += 1
+
+            j += 1
+        if ( match != cols ):
+            dataUnion.insert(len(dataUnion), table1[rowsI])
+            
+        rowsI += 1
+        
+
+union (GRADUATES, MANAGERS)
+#print(sorted(GRADUATES, key=lambda Age: Age[2]))
